d002b50ba5d304df64f9a00bc5532b74a44ba6b2
ko1307 <208754810+13ko13@users.noreply.github.com>
2025-12-15 18:53:44 +0900

    カメラのカプセル化
    カプセル化し忘れていたのでカプセル化を行った

--- Transforman/Graphics/Camera.h
+++ Transforman/Graphics/Camera.h
@@ -19,23 +19,21 @@ public:
 	/// カメラが衝撃を受けたときの関数
 	/// </summary>
 	void OnImpact();
-	/// <summary>
-	/// 
-	/// </summary>
-	void Impact();
-
-	//画面左上のワールド座標(ピクセル)を返す
-	//横スクロールに必要な開始タイル列と半端オフセット計算に使う
-	float GetWorldOriginX() const;
-	float GetWorldOriginY() const;
 
 	void OnArriveEnemy(std::shared_ptr<Player> pPlayer,std::shared_ptr<Stage> pStage);
 	Vector2 GetPos() { return m_pos; };
 
 private:
+	//変数
 	Vector2 m_pos;	//実際のカメラのポジション
 	Vector2 m_drawOffset;	//全てのDrawObjectに足す値
 	bool m_isArrive;//ボスの場所に到着したかどうか
 	int m_shakingFrame;//現在のカメラを揺らしている時間
+
+	//関数
+	/// <summary>
+	/// カメラに衝撃を与える関数
+	/// </summary>
+	void Impact();
 };
 
