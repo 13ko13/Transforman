8ede1be446553270513b394fb76c97441f4fda60
ko1307 <208754810+13ko13@users.noreply.github.com>
2025-12-18 00:55:19 +0900

    Textクラス作成準備

--- Transforman/Text.cpp
+++ Transforman/Text.cpp
@@ -1,1 +1,22 @@
 #include "Text.h"
+
+Text::Text(TextType textType) :
+	m_type(textType)
+{
+
+}
+
+Text::~Text()
+{
+
+}
+
+void Text::Update()
+{
+
+}
+
+void Text::Draw()
+{
+
+}
--- Transforman/Text.h
+++ Transforman/Text.h
@@ -1,9 +1,26 @@
 #pragma once
+#include "Utility/Geometry.h"
+
+enum class TextType
+{
+	Move,
+	Jump,
+	Shot,
+	ChargeShot,
+	Flame,
+	Parry
+};
+
 class Text
 {
 public:
+	Text(TextType textType);
+	~Text();
 	void Update();
 	void Draw();
 private:
-};
+	Vector2 m_pos;//位置
+	int handle;//画像ハンドル
 
+	TextType m_type;//どのテキスト(画像)を表示させるか
+};
