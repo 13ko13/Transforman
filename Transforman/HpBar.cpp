e2e0756ea5c703aee710e81d712a19d586ecf962
ko1307 <208754810+13ko13@users.noreply.github.com>
2026-01-09 23:39:31 +0900

    HPバーを表示できるようにした

--- Transforman/HpBar.cpp
+++ Transforman/HpBar.cpp
@@ -4,11 +4,23 @@
 
 namespace
 {
-	constexpr int graph_width = 60;
-	constexpr int graph_height = 30;
+	constexpr int graph_width = 60;//HPバーと幅
+	constexpr int graph_height = 30;//HPバーと高さ
+
+	//描画位置オフセット
+	constexpr int draw_offset_x = 170;
+	constexpr int draw_offset_y = 50;
+
+	constexpr int pinch_hp = 2;//ピンチと判断するHP
+	constexpr int normal_hp = 3;//通常と判断するHP
+
+	constexpr int normal_hp_offset = 95;//通常HPの描画位置オフセット
+	constexpr int next_hp_offset = 25;//次のHPの描画位置オフセット
+
+	constexpr double graph_size = 5.0; //拡大率
 }
 
-HpBar::HpBar(int maxHitPoint,int srcX,int srcY) :
+HpBar::HpBar(int maxHitPoint) :
 	UserInterface({0,0}),
 	m_hitPoint(0),
 	m_maxHitPoint(maxHitPoint)
@@ -18,20 +30,24 @@ HpBar::HpBar(int maxHitPoint,int srcX,int srcY) :
 	m_handles.push_back(handle);
 	assert(handle >= 0);//ハンドルnullチェック
 
-	handle = LoadGraph("img/game/Player/HP_Bar_Background");//HPバーの背景
+	handle = LoadGraph("img/game/Player/HP_Bar_Background.png");//HPバーの背景
+	m_handles.push_back(handle);
 	assert(handle >= 0);//ハンドルnullチェック
 
-	handle = LoadGraph("img/game/Player/HP_Bar_Normal");//HPバーの通常カラー
+	handle = LoadGraph("img/game/Player/HP_Bar_Heart.png");//HPバーのハート
+	m_handles.push_back(handle);
 	assert(handle >= 0);//ハンドルnullチェック
 
-	handle = LoadGraph("img/game/Player/HP_Bar_Pinch");//HPバーのピンチ時カラー
+	handle = LoadGraph("img/game/Player/HP_Bar_Normal.png");//HPバーの通常カラー
+	m_handles.push_back(handle);
+	assert(handle >= 0);//ハンドルnullチェック
+
+	handle = LoadGraph("img/game/Player/HP_Bar_Pinch.png");//HPバーのピンチ時カラー
+	m_handles.push_back(handle);
 	assert(handle >= 0);//ハンドルnullチェック
 	
 	//HPを設定
 	m_hitPoint = m_maxHitPoint;
-	//切り取りサイズを設定
-	m_srcX = srcX;
-	m_srcY = srcY;
 }
 
 HpBar::~HpBar()
@@ -49,15 +65,60 @@ void HpBar::Update(std::shared_ptr<Player> pPlayer)
 
 void HpBar::Draw()
 {
+#ifdef _DEBUG
+	DrawFormatString(0, 200, 0xffffff, "PlayerHitPoint : %d", m_hitPoint);
+#endif // _DEBUG
+
+	////UIの描画
 	
-	for (auto& handle : m_handles)
+	//HPの枠の描画
+	DrawRotaGraph(
+		static_cast<int>(m_pos.x + draw_offset_x),
+		static_cast<int>(m_pos.y + draw_offset_y),
+		5.0, 0.0,
+		m_handles[static_cast<int>(handleNomber::Frame)],
+		true, false);
+
+	//ハートの描画
+	DrawRotaGraph(
+		static_cast<int>(m_pos.x + draw_offset_x),
+		static_cast<int>(m_pos.y + draw_offset_y),
+		5.0, 0.0,
+		m_handles[static_cast<int>(handleNomber::Heart)],
+		true, false);
+
+	switch (m_hitPoint)
 	{
-		////UIの描画
-		//DrawRectRotaGraph(
-		//	static_cast<int>(m_pos.x),
-		//	static_cast<int>(m_pos.y),
-		//	m_srcX, m_srcY,
-		//	);
+	case 1:
+	case 2:
+		for (int i = 0; i < m_hitPoint; ++i)
+		{
+			//ピンチ時のHPの描画
+			DrawRotaGraph(
+				static_cast<int>(m_pos.x + draw_offset_x + i * next_hp_offset),
+				static_cast<int>(m_pos.y + draw_offset_y),
+				graph_size, 0.0,
+				m_handles[static_cast<int>(handleNomber::Pinch)],
+				true, false);
+		}
+		break;
+	case 3:
+	case 4:
+	case 5:
+		for (int i = 0; i < m_hitPoint; ++i)
+		{
+			//ノーマルHP描画
+			DrawRotaGraph(
+				static_cast<int>(m_pos.x + normal_hp_offset + i * next_hp_offset),
+				static_cast<int>(m_pos.y + draw_offset_y),
+				graph_size, 0.0,
+				m_handles[static_cast<int>(handleNomber::Normal)],
+				true, false);
+		}
+		break;
 	}
+	//HPバーの描画
+	
+
 	
 }
--- Transforman/HpBar.h
+++ Transforman/HpBar.h
@@ -7,7 +7,7 @@
 class HpBar : public UserInterface
 {
 public:
-	HpBar(int maxHitPoint,int srcX, int srcY);
+	HpBar(int maxHitPoint);
 	~HpBar();
 
 	void Update() override;
@@ -19,13 +19,12 @@ private:
 	{
 		Frame,//枠
 		Background,//背景
+		Heart,//ハート
 		Normal,//通常HP
-		pinch//ピンチ時
+		Pinch//ピンチ時
 	};
 	std::vector<int> m_handles;//ヒットポイントの画像ハンドル
 	int m_hitPoint;//現在のHP
 	int m_maxHitPoint;//最大HP
-	int m_srcX;//横切り取りサイズ
-	int m_srcY;//縦切り取りサイズ
 };
 
