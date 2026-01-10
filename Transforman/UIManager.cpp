f37bc0ee3ab61993bfa4e0548dcf03d7f39ebef4
ko1307 <208754810+13ko13@users.noreply.github.com>
2026-01-11 02:34:46 +0900

    チャージショットボスのUIを表示できるようにした

--- Transforman/UIManager.cpp
+++ Transforman/UIManager.cpp
@@ -1,24 +1,34 @@
 #include "UIManager.h"
 #include "Objects/Player.h"
+#include "Objects/ChargeShotBoss.h"
+#include "PlayerHpBar.h"
+#include "EnemyHpBar.h"
 
-UIManager::UIManager(std::shared_ptr<Player> pPlayer)
+UIManager::UIManager(std::shared_ptr<Player> pPlayer, std::shared_ptr<ChargeShotBoss> pChargeBoss)
 {
 	const int playerHP = pPlayer->GetHitPoint();
 	playerHpBar = std::make_shared<PlayerHpBar>(playerHP);
+
+	const int enemyHP = pChargeBoss->GetHitPoint();
+	enemyHpBar = std::make_shared<EnemyHpBar>(enemyHP);
 }
 
 UIManager::~UIManager()
 {
 }
 
-void UIManager::Update(std::shared_ptr<Player> pPlayer)
+void UIManager::Update(std::shared_ptr<Player> pPlayer, std::shared_ptr<ChargeShotBoss> pChargeBoss)
 {
 	//プレイヤーのHPバーの更新
 	playerHpBar->Update(pPlayer);
+	//エネミーのHPバーの更新
+	enemyHpBar->Update(pChargeBoss->GetHitPoint());
 }
 
 void UIManager::Draw()
 {
 	//プレイヤーのHPバーの描画
 	playerHpBar->Draw();
+	//エネミーのHPバーの描画
+	enemyHpBar->Draw();
 }
--- Transforman/UIManager.h
+++ Transforman/UIManager.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <memory>
-#include "PlayerHpBar.h"
 
 class Player;
+class PlayerHpBar;
+class EnemyHpBar;
+class ChargeShotBoss;
 class UIManager
 {
 public:
-	UIManager(std::shared_ptr<Player> pPlayer);
+	UIManager(std::shared_ptr<Player> pPlayer, std::shared_ptr<ChargeShotBoss> pChargeBoss);
 	~UIManager();
 
-	void Update(std::shared_ptr<Player> pPlayer);
+	void Update(std::shared_ptr<Player> pPlayer, std::shared_ptr<ChargeShotBoss> pChargeBoss);
 	void Draw();
 
 private:
 	std::shared_ptr<PlayerHpBar> playerHpBar;
+	std::shared_ptr<EnemyHpBar> enemyHpBar;	
 };
 
