ed2609c343133ad1658aadcb4149fcacf7004396
ko1307 <208754810+13ko13@users.noreply.github.com>
2026-01-11 02:37:57 +0900

    上手くいかなかったのでプレイヤーと同じ処理をコピペ

--- Transforman/EnemyHpBar.h
+++ Transforman/EnemyHpBar.h
@@ -1,5 +1,28 @@
 #pragma once
-class EnemyHpBar
+#include <vector>
+#include "UserInterface.h"
+
+class EnemyHpBar : public UserInterface
 {
+public:
+	EnemyHpBar(int maxHP);
+	~EnemyHpBar();
+	
+	void Update(int currentHP);
+	void Update() override;
+	void Draw() override;
+private:
+	enum class HandleNomber
+	{
+		Frame,//枠
+		Heart,//ハート
+		Normal,//通常HP
+		Pinch,//ピンチ時
+		Empty,//空HP
+	};
+
+	int m_maxHP;
+	int m_currentHP;
+	std::vector<int> m_handles;//ヒットポイントの画像ハンドル
 };
 
